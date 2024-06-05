1af301025e489aa6f6d0f371a58b8982ee7cb683
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-05 09:49:44 +0330

    update forum/main_app/models.py: expelenation: create our models => Board, Topic, Post, User

--- forum/main_app/models.py
+++ forum/main_app/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_update = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name='topics', null=True, on_delete=models.SET_NULL)
+    starter = models.ForeignKey(User, related_name='topics', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.subject
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=5000)
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+
