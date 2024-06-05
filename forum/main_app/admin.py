18c8429d69d5abe330a4f668235c611a4e78a49a
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-05 12:53:11 +0330

    update forum/main_app/admin.py: 1) add models to admin page

--- forum/main_app/admin.py
+++ forum/main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Topic, Board, Post
 
 # Register your models here.
+
+
+main_app_models = [Board, Topic, Post]
+admin.site.register(main_app_models)
