cc273d61245bad49da2b4c9f3c03ce75fd5d3d81
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-08 11:16:29 +0330

    update accounts/tests.py: write some test for signup route

--- forum/accounts/tests.py
+++ forum/accounts/tests.py
@@ -1,3 +1,17 @@
+# dj
+from django.urls import resolve, reverse
 from django.test import TestCase
 
-# Create your tests here.
+# internal
+from .views import signup
+
+
+class SignUpTests(TestCase):
+    def test_signup_status_code(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_url_resolves_signup_view(self):
+        view = resolve('/signup/')
+        self.assertEqual(view.func, signup)
