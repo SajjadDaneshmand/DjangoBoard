1378c70991a3540cbe708e3b3bc61b6a345c4f64
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-08 20:06:09 +0330

    create successfully sign-up page

--- forum/accounts/views.py
+++ forum/accounts/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+# dj
+from django.contrib.auth import login as auth_login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+# internal
+from .forms import SignUpForm
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
