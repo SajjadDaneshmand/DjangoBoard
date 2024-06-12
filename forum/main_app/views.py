00c8b46ac316b703337f3f92b22723393a949b78
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-12 17:06:14 +0330

    update main_app/views.py: 1) set login required for those not log-in. it set for new_topic view

--- forum/main_app/views.py
+++ forum/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Board, Topic, Post
 
@@ -18,6 +19,7 @@ def board_topics(request, pk):
     return render(request, 'topics.html', {'board': board})
 
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
     user = User.objects.first()  # TODO: get the currently logged in user
