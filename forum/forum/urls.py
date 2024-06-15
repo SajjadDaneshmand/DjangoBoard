5232849b1a2a8f89831b8859f766599cfe95846b
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-15 19:17:18 +0330

    define new routes (for my account) or replace function defined in each route with class view

--- forum/forum/urls.py
+++ forum/forum/urls.py
@@ -10,11 +10,16 @@ from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main_app_views.home, name='home'),
-    path('boards/<int:pk>/', main_app_views.board_topics, name='board_topics'),
+    path('', main_app_views.BoardListView.as_view(), name='home'),
+    path('boards/<int:pk>/', main_app_views.TopicListView.as_view(), name='board_topics'),
     path('boards/<int:pk>/new/', main_app_views.new_topic, name='new_topic'),
-    path('boards/<int:pk>/topics/<int:topic_pk>/', main_app_views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/', main_app_views.PostListView.as_view(), name='topic_posts'),
     path('boards/<int:pk>/topics/<int:topic_pk>/reply/', main_app_views.reply_topic, name='reply_topic'),
+    path(
+        'boards/<int:pk>/topics/<int:topic_pk>/posts/<int:post_pk>/edit/',
+        main_app_views.PostUpdateView.as_view(),
+        name='edit_post'
+    ),
     path('signup/', accounts_views.signup, name='signup'),
     path('logout/', accounts_views.user_logout, name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
@@ -44,4 +49,5 @@ urlpatterns = [
         template_name='password_change_done.html'
     ),
          name='password_change_done'),
+    path('settings/account/', accounts_views.UserUpdateView.as_view(), name='my_account'),
 ]
